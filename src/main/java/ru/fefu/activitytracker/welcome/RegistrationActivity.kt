71d0ef687672a99229f739410f1142f23f18134b
Khol2721 <49965677+Khol2721@users.noreply.github.com>
2021-12-25 21:25:17 +1000

    Add files via upload

---
+++ src/main/java/ru/fefu/activitytracker/welcome/RegistrationActivity.kt
@@ -0,0 +1,19 @@
+package ru.fefu.activitytracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+    }
+
+    fun back (view: View) {
+        val bk = Intent(this, WelcomeActivity::class.java)
+        startActivity(bk)
+    }
+}
