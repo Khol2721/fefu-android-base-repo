9a2c0e21137347dc3f8a0005815427152baec23b
Khol2721 <49965677+Khol2721@users.noreply.github.com>
2021-12-29 00:35:36 +1000

    Add files via upload

---
+++ app/src/main/java/ru/fefu/activitytracker/main_page/MainActivity.kt
@@ -0,0 +1,66 @@
+package ru.fefu.activitytracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.fefu.activitytracker.R
+import ru.fefu.activitytracker.databinding.ActivityMainBinding
+import java.lang.reflect.Array.newInstance
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().apply {
+                add(
+                    R.id.fragmentContainerView,
+                    ActivityFragment(),
+                    "activityFragment"
+                )
+                commit()
+            }
+        }
+
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            val activityFragment = supportFragmentManager.findFragmentByTag("activityFragment")
+            val profileFragment = supportFragmentManager.findFragmentByTag("profileFragment")
+            when (it.itemId) {
+                R.id.activity -> {
+                    if (profileFragment !== null) {
+                        supportFragmentManager.beginTransaction().hide(profileFragment).commit()
+                    }
+                    if (activityFragment !== null) {
+                        supportFragmentManager.beginTransaction().show(activityFragment).commit()
+                    }
+                }
+
+                R.id.profile -> {
+                    if (activityFragment !== null) {
+                        supportFragmentManager.beginTransaction().hide(activityFragment).commit()
+                    }
+                    if (profileFragment !== null) {
+                        supportFragmentManager.beginTransaction().show(profileFragment).commit()
+                    } else {
+                        supportFragmentManager.beginTransaction().apply {
+                            add(
+                                R.id.fragmentContainerView,
+                                ProfileFragment(),
+                                "profileFragment"
+                            )
+                            commit()
+                        }
+                    }
+                }
+            }
+            true
+        }
+    }
+}
