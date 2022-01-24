4fd235a77d769edf1523651d40e372abaab5a3cc
Khol2721 <aburachinskiy@yandex.ru>
2022-01-24 15:46:15 +0300

    fix

--- app/src/main/java/ru/fefu/activitytracker/main_page/lists/ActivityType.kt
+++
@@ -1,6 +0,0 @@
-package ru.fefu.activitytracker.main_page.lists
-
-data class ActivityType (
-    val activity: String,
-    var isSelected: Boolean,
-)
--- app/src/main/java/ru/fefu/activitytracker/main_page/room/ActivityTypes.kt
+++
@@ -1,7 +0,0 @@
-package ru.fefu.activitytracker.main_page.room
-
-enum class ActivityTypes(val type: String) {
-    BICYCLE("Велосипед"),
-    RUNNING("Бег"),
-    STEP("Шаг"),
-}
